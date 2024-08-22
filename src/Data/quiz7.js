7f163c4fccae94db151d53c940d5697c45ebfa78
nam2000808 <129300795+nam2000808@users.noreply.github.com>
2024-08-22 22:52:28 +0700

    Update quiz7.js

--- src/Data/quiz7.js
+++ src/Data/quiz7.js
@@ -1,1 +1,152 @@
+const quiz7 = [
+ {
+question: "Theo Hiến pháp năm 2013, nước Cộng hòa xã hội chủ nghĩa Việt Nam bao gồm những bộ phận lãnh thổ nào?",
+image: "",
+choices: [
+"Đất liền, hải đảo, vùng biển và vùng trời",
+"Đất liền, hải đảo và vùng biển",
+"Đất liền, hải đảo và vùng trời",
+"Đất liền và các hải đảo"
+],
+correct: "Đất liền, hải đảo, vùng biển và vùng trời",
+explanation: "Điều 5 của Hiến pháp năm 2013 quy định rõ: 'Nước Cộng hòa xã hội chủ nghĩa Việt Nam là một, thống nhất và toàn vẹn lãnh thổ, bao gồm vùng đất, vùng trời, vùng biển và thềm lục địa.'"
+},
 
+{
+question: "Theo Hiến pháp năm 2013, nhà nước Cộng hòa xã hội Chủ nghĩa Việt Nam là nhà nước pháp quyền xã hội chủ nghĩa của ai, do ai, vì ai?",
+image: "",
+choices: [
+"Của nông dân, do nông dân, vì nông dân",
+"Của trí thức, do trí thức, vì trí thức",
+"Của Nhân dân, do Nhân dân, vì Nhân dân",
+"Của công nhân, do công nhân, vì công nhân"
+],
+correct: "Của Nhân dân, do Nhân dân, vì Nhân dân",
+explanation: "Điều 2 của Hiến pháp năm 2013 khẳng định: 'Nước Cộng hòa xã hội chủ nghĩa Việt Nam là Nhà nước pháp quyền xã hội chủ nghĩa của Nhân dân, do Nhân dân, vì Nhân dân.'"
+},
+{
+question: "Theo Hiến pháp năm 2013, nước Cộng hòa xã hội chủ nghĩa Việt Nam do ai làm chủ?",
+image: "",
+choices: [
+"Nhân dân",
+"Nhà nước",
+"Quốc hội",
+"Chính phủ"
+],
+correct: "Nhân dân",
+explanation: "Điều 2 của Hiến pháp năm 2013 khẳng định: 'Nước Cộng hòa xã hội chủ nghĩa Việt Nam là Nhà nước pháp quyền xã hội chủ nghĩa của Nhân dân, do Nhân dân, vì Nhân dân. Nhân dân làm chủ Nhà nước.'"
+},
+{
+question: "Theo Hiến pháp năm 2013, tất cả quyền lực của nhà nước Cộng hòa xã hội chủ nghĩa Việt Nam thuộc về ai?",
+image: "",
+choices: [
+"Trí thức",
+"Nhân dân",
+"Công nhân",
+"Nông dân"
+],
+correct: "Nhân dân",
+explanation: "Điều 2 của Hiến pháp năm 2013 nêu rõ: 'Tất cả quyền lực nhà nước thuộc về Nhân dân mà nền tảng là liên minh giữa giai cấp công nhân với giai cấp nông dân và đội ngũ trí thức.'" 1 
+},
+{
+question: "Theo Hiến pháp năm 2013, quyền làm chủ của nhân dân được tổ chức nào bảo đảm?",
+image: "",
+choices: [
+"Nhà nước",
+"Quốc hội",
+"Chính phủ",
+"Mặt trận tổ quốc Việt Nam"
+],
+correct: "Nhà nước",
+explanation: "Điều 2 của Hiến pháp năm 2013 quy định: 'Nhà nước bảo đảm các quyền cơ bản của công dân; thực hiện mục tiêu dân giàu, nước mạnh, dân chủ, công bằng, văn minh; bảo đảm quyền làm chủ của Nhân dân.'"
+},
+
+{
+question: "Theo Hiến pháp năm 2013, quyền con người được tổ chức nào công nhận, tôn trọng, bảo vệ và bảo đảm?",
+image: "",
+choices: [
+"Quốc hội",
+"Chính phủ",
+"Mặt trận tổ quốc Việt Nam",
+"Nhà nước"
+],
+correct: "Nhà nước",
+explanation: "Điều 11 của Hiến pháp năm 2013 quy định: 'Ở nước Cộng hòa xã hội chủ nghĩa Việt Nam, các quyền con người, quyền công dân được công nhận, tôn trọng, bảo vệ và bảo đảm theo Hiến pháp và pháp luật.'"
+},
+
+{
+question: "Theo Hiến pháp năm 2013, quyền công dân được tổ chức nào công nhận, tôn trọng, bảo vệ và bảo đảm?",
+image: "",
+choices: [
+"Chính phủ",
+"Mặt trận tổ quốc Việt Nam",
+"Nhà nước",
+"Quốc hội"
+],
+correct: "Nhà nước",
+explanation: "Điều 11 của Hiến pháp năm 2013 quy định: 'Ở nước Cộng hòa xã hội chủ nghĩa Việt Nam, các quyền con người, quyền công dân được công nhận, tôn trọng, bảo vệ và bảo đảm theo Hiến pháp và pháp luật.'"
+},
+{
+question: "Theo Hiến pháp năm 2013 thì việc bầu cử đại biểu Quốc hội và đại biểu Hội đồng nhân dân được tiến hành theo nguyên tắc nào?",
+image: "",
+choices: [
+"Phổ thông, bình đẳng, trực tiếp và bỏ phiếu kín",
+"Công khai, dân chủ, khách quan và bỏ phiếu kín",
+"Dân chủ, bình đẳng, khách quan và bỏ phiếu kín",
+"Phổ thông, dân chủ, khách quan và bỏ phiếu kín"
+],
+correct: "Phổ thông, bình đẳng, trực tiếp và bỏ phiếu kín",
+explanation: "Điều 6 của Hiến pháp năm 2013 quy định: 'Việc bầu cử đại biểu Quốc hội và đại biểu Hội đồng nhân dân được tiến hành theo nguyên tắc phổ thông, bình đẳng, trực tiếp và bỏ phiếu kín.'"
+},
+{
+question: "Hiến pháp năm 2013 quy định nhà nước Cộng hòa xã hội Chủ nghĩa Việt Nam được tổ chức và hoạt động như thế nào?",
+image: "",
+choices: [
+"Theo pháp luật",
+"Theo Hiến pháp và pháp luật",
+"Theo chủ trương, đường lối của Đảng",
+"Theo Hiến pháp"
+],
+correct: "Theo Hiến pháp và pháp luật",
+explanation: "Điều 4 của Hiến pháp năm 2013 quy định: 'Nhà nước Cộng hòa xã hội chủ nghĩa Việt Nam là Nhà nước pháp quyền xã hội chủ nghĩa của Nhân dân, do Nhân dân, vì Nhân dân. Nhà nước bảo đảm các quyền cơ bản của công dân; thực hiện mục tiêu dân giàu, nước mạnh, dân chủ, công bằng, văn minh; bảo đảm quyền làm chủ của Nhân dân. Nhà nước Cộng hòa xã hội chủ nghĩa Việt Nam được tổ chức và hoạt động theo Hiến pháp và pháp luật.'"
+}  ,
+{
+question: "Theo Hiến pháp năm 2013, nhà nước Cộng hòa xã hội Chủ nghĩa Việt Nam quản lý xã hội bằng gì?",
+image: "",
+choices: [
+"Chủ trương, đường lối chính sách của Đảng",
+"Pháp luật",
+"Hiến pháp",
+"Hiến pháp và pháp luật"
+],
+correct: "Pháp luật",
+explanation: "Điều 12 của Hiến pháp năm 2013 quy định: 'Nhà nước Cộng hòa xã hội chủ nghĩa Việt Nam quản lý xã hội bằng pháp luật, xây dựng Nhà nước pháp quyền xã hội chủ nghĩa; bảo đảm an ninh quốc gia, trật tự, an toàn xã hội; tôn trọng và bảo vệ quyền con người, quyền công dân.'"
+},
+{
+question: "Theo Hiến pháp năm 2013, tổ chức nào là cơ sở chính trị của chính quyền nhân dân?",
+image: "",
+choices: [
+"Hội Liên hiệp phụ nữ Việt Nam",
+"Hội Cựu chiến binh Việt Nam",
+"Mặt trận Tổ quốc Việt Nam",
+"Hội Nông dân Việt Nam"
+],
+correct: "Mặt trận Tổ quốc Việt Nam",
+explanation: "Điều 7 của Hiến pháp năm 2013 quy định: 'Mặt trận Tổ quốc Việt Nam và các tổ chức chính trị - xã hội thành viên là cơ sở chính trị của chính quyền nhân dân, là cầu nối giữa Đảng, Nhà nước với Nhân dân.'"
+},
+
+{
+question: "Theo Hiến pháp năm 2013, tổ chức nào đại diện, bảo vệ quyền và lợi ích hợp pháp, chính đáng của Nhân dân?",
+image: "",
+choices: [
+"Hội Cựu chiến binh Việt Nam",
+"Mặt trận Tổ quốc Việt Nam",
+"Hội Nông dân Việt Nam",
+"Hội Liên hiệp phụ nữ Việt Nam"
+],
+correct: "Mặt trận Tổ quốc Việt Nam",
+explanation: "Điều 7 của Hiến pháp năm 2013 quy định một trong những chức năng của Mặt trận Tổ quốc Việt Nam là 'đại diện, bảo vệ quyền và lợi ích hợp pháp, chính đáng của Nhân dân.'"
+},
+  
+];
+export default quiz7;
